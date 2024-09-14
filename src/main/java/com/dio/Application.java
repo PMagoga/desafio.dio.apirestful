f0d53f0de30692dfce74673bb85aef4ed2bb3acb
PMagoga <pmagoga@gmail.com>
2024-09-14 16:01:19 -0300

    mudando perfil de desenvolvimento

--- src/main/java/com/dio/Application.java
+++ src/main/java/com/dio/Application.java
@@ -3,14 +3,17 @@ package com.dio;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+@EnableAutoConfiguration
 @OpenAPIDefinition(servers = { @Server(url = "/", description = "Default server URL")})
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
 	}
 
 }
